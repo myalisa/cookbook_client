75cc0a7590005fa42c83280a866ca0eab1ae29da
myalisa <myalisamiro@gmail.com>
2019-09-24 11:25:08 -0500

    Updated Views

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -6,15 +6,55 @@ class Client::RecipesController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    
+    response = HTTP.post(
+                  "http://localhost:3000/api/recipes", 
+                    form: {
+                    title: params[:title],
+                    chef: params[:chef],
+                    ingredients: params[:ingredients],
+                    directions: params[:directions],
+                    prep_time: params[:prep_time],
+                    image_url: params[:image_url]
+            } )
+
+    @recipe = response.parse
+
+    redirect_to "/client/recipes/#{@recipe["id"]}"
+  end
+
   def show
     response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
     @recipe = response.parse
     render 'show.html.erb'
   end
 
-  def new
-    render 'new.html.erb'
-    
+  def edit
+    response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
+    @recipe = response.parse
+    render 'edit.html.erb'
+  end
+
+  def update
+    response = HTTP.patch(
+                          "http://localhost:3000/api/recipes/#{params[:id]}",
+                          form: {
+                                  title: params[:title],
+                                  chef: params[:chef],
+                                  prep_time: params[:prep_time],
+                                  ingredients: params[:ingredients],
+                                  directions: params[:directions],
+                                  image_url: params[:image_url]
+
+                                }
+                                )
+    @recipe = response.parse
+    redirect_to "/client/recipes/#{@recipe['id']}"
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :client do
     get '/recipes' => 'recipes#index'
-    get '/recipes/:id' => 'recipes#show'
     get '/recipes/new' => 'recipes#new'
+    post '/recipes' => 'recipes#create'
+    get '/recipes/:id' => 'recipes#show'
+    get '/recipes/:id/edit' => 'recipes#edit'
+    patch '/recipes/:id' => 'recipes#update'
+
   end
 end
